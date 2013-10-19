cadd3324ecb01eb36369f260e7b6e478c417f6d4
v3rron <go7hic@gmail.com>
2013-10-19 19:53:23 -0400

    logo changed, email address changed, name changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,6 @@ Backstage::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'stark-hamlet-9784.herokuapp.com' }
+  #config.action_mailer.default_url_options = { :host => 'stark-hamlet-9784.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'art-academy.com.ua' }
 end
